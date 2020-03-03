ec95e5adb3ee78ef1cfc444a530911ada491442c
attriaayush <aayush.attri@red-badger.com>
2020-03-03 16:22:06 +0000

    adding email field to the login page

--- UberClone/Controller/LoginController.swift
+++ UberClone/Controller/LoginController.swift
@@ -12,7 +12,7 @@ class LoginController: UIViewController {
     
     // MARK: - Properties
     // here we are creating a titleLabel and casting it to UILabel
-    private let titleLable: UILabel = {
+    private let titleLabel: UILabel = {
         let label = UILabel() // init
         // Creating some properties for the label
         label.text = "UBER"
@@ -21,17 +21,53 @@ class LoginController: UIViewController {
         return label
     }()
     
+    private lazy var emailContainerView: UIView = {
+        let view = UIView()
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "ic_mail_outline_white_2x")
+        imageView.alpha = 0.87
+        
+        view.addSubview(imageView)
+        imageView.centerY(inView: view)
+        imageView.anchor(left: view.leftAnchor, paddingLeft: 8, width: 24, height: 24)
+        
+        view.addSubview(emailTextField)
+        emailTextField.centerY(inView: view)
+        emailTextField.anchor(left: imageView.rightAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingLeft: 8, paddingBottom: 8)
+        
+        let seperatorView = UIView()
+        seperatorView.backgroundColor = .lightGray
+        view.addSubview(seperatorView)
+        seperatorView.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingLeft: 8, paddingBottom: 0.75)
+
+        return view
+    }()
+    
+    private let emailTextField: UIView = {
+        let textField = UITextField()
+        
+        textField.borderStyle = .none
+        textField.font = UIFont.systemFont(ofSize: 16)
+        textField.textColor = .white
+        textField.keyboardAppearance = .dark
+        textField.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [NSAttributedString.Key.foregroundColor : UIColor.lightGray])
+        
+        return textField
+    }()
+    
+    
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.init(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
         
-        view.addSubview(titleLable)
-        titleLable.translatesAutoresizingMaskIntoConstraints = false // disabling the autoresizing contraints
-        // programtically centering a label
-        titleLable.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        titleLable.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+        view.addSubview(titleLabel)
+        titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor)
+        titleLabel.centerX(inView: view)
+        
+        view.addSubview(emailContainerView)
+        emailContainerView.anchor(top: titleLabel.topAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 40, paddingLeft: 16, paddingRight: 16, height: 50)
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
